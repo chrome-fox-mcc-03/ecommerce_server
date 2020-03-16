fd1b47d8f64adb7f209008b8aa441220d3912249
alvinarkansas <alvinarkansas@gmail.com>
2020-03-16 12:32:50 +0700

    TDD setup

---
+++ models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  User.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'User'
+  })
+  
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
---
+++ test/user.test.js
@@ -0,0 +1,24 @@
+// import { request } from "supertest";
+// import { app } from "../app";
+// const { sequelize } = require('../models');
+// const { queryInterface } = sequelize;
+
+// let dummySignUp = {
+//     name: 'Hannah',
+//     email: 'hannah@gmail.com',
+//     password: '123123'
+// }
+
+// describe('POST /register', () => {
+//     describe('success process', () => {
+//         test('Should send an object (token, name) with status code 201', (done) => {
+//             // = = = = hit server = = = = 
+//             request(app)
+//                 .post('/register')
+//                 .send(dummySignUp)
+//                 .end((err, res) => {
+//                     // expect
+//                 })
+//         })
+//     })
+// })

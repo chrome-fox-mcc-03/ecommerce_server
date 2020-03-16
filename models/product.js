a2cec8c0f1c9c7b8412282e9013605d4ae9f268c
thejatmik <jatmikatejasukmana@gmail.com>
2020-03-16 18:04:40 +0700

    Initialize product testing

---
+++ models/product.js
@@ -0,0 +1,93 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  class Product extends sequelize.Sequelize.Model {};
+  Product.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: "product name is required",
+        },
+        notEmpty: {
+          args: true,
+          msg: "product name is required",
+        },
+      }
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: {
+          args: true,
+          msg: "invalid image url",
+        }
+      }
+    },
+    price: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: "product price is required",
+        },
+        notEmpty: {
+          args: true,
+          msg: "product price is required",
+        },
+        isNumeric: {
+          args: true,
+          msg: "invalid price value",
+        },
+        isInt: {
+          args: true,
+          msg: "invalid price format",
+        },
+        min: {
+          args: 0,
+          msg: "price must be positive value or zero",
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: "product stock is required",
+        },
+        notEmpty: {
+          args: true,
+          msg: "product stock is required",
+        },
+        isNumeric: {
+          args: true,
+          msg: "stock amount must be number",
+        },
+        isInt: {
+          args: true,
+          msg: "stock amount must be integer",
+        },
+        min: {
+          args: 0,
+          msg: "invalid stock amout",
+        },
+      }
+    },
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: "Product"
+  });
+  Product.associate = function(models) {
+    // associations can be defined here
+    Product.belongsTo(models.User);
+  };
+  return Product;
+};
--- models/user.js
+++ models/user.js
@@ -53,6 +53,7 @@ module.exports = (sequelize, DataTypes) => {
   });
   User.associate = function(models) {
     // associations can be defined here
+    User.hasMany(models.Product);
   };
   return User;
 };

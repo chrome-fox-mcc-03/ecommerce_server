c8b6827346f8d11192bcccd8e760bb4579d80088
Juli Yanti <juleshwa@gmail.com>
2020-03-17 16:51:38 +0700

    Controllers update

--- controllers/Cart.js
+++ controllers/Cart.js
@@ -1,9 +1,137 @@
 'use strict'
 
+const { Product, Cart, sequelize } = require('../models')
+
 
 class CartController {
 
+    static createCart(req, res, next) {
+        const UserId = req.loginId;
+        const payload = {
+            quantity: req.body.quantity,
+            cart_status: req.body.cart_status,
+            ProductId: req.body.ProductId,
+        }
 
+        Product.findOne({
+            where: {
+                id: ProductId
+            }
+        }).then(product => {
+            payload.UserId = UserId;
+            payload.price = payload.quantity * product.price;
+            return [product.name, Cart.create(payload)]
+        }).then(result => {
+            console.log(result)
+        }).catch(next)
+
+    }
+
+    static checkOut(req, res, next) {
+        const UserId = req.loginId;
+        const { carts } = req.body;
+        const selected = [];
+        const updateSelected = [];
+        const cartSelected = [];
+        let newSelect;
+
+        sequelize.transaction(t => {
+            carts.forEach(cart => {
+                newSelect = Product.findOne({
+                    where: {
+                        id: cart.ProductId
+                    }, transaction: t
+                });
+                selected.push(newSelect)
+            });
+
+            return Promise.all(selected).then(products => {
+
+                products.forEach((product, index) => {
+                    newSelect = Product.update({
+                        name: product.name,
+                        price: product.price,
+                        stock_quantity: product.stock_quantity - carts[index].quantity
+                    }, {
+                        where: {
+                            id: product.id
+                        }, transaction: t
+                    });
+                    updateSelected.push(newSelect);
+                });
+
+                return Promise.all(updateSelected).then(products => {
+                    carts.forEach(cart => {
+                        newSelect = Cart.update({
+                            id: cart.id,
+                            price: cart.price * cart.quantity,
+                            quantity: cart.quantity,
+                            cart_status: true,
+                            UserId: cart.UserId,
+                            ProductId: cart.ProductId
+
+                        }, {
+                            where: {
+                                id: cart.id
+                            },
+                            transaction: t
+                        })
+                        cartSelected.push(newSelect);
+                    });
+                    return Promise.all(cartSelected)
+                })
+
+
+            });
+
+
+
+        }).then(result => {
+            console.log(result)
+        }).catch(next);
+    }
+
+    static cartUpdate(req, res, next) {
+
+    }
+
+    static emptyCart(req, res, next) {
+        const { id } = req.params;
+        Cart.destroy({
+            where: {
+                id
+            }
+        }).then(result => {
+
+        }).catch(next);
+
+    }
+
+    static findRecentProducts(req, res, next) {
+        const UserId = req.loginId;
+        Cart.findAll({
+            include: [{
+                model: Product,
+                attributes: ['name', 'price']
+            }],
+            where: {
+                UserId,
+                cart_status: false
+            },
+            order: [['id', 'ASC']]
+        }).then(carts => {
+            if (carts) {
+                console.log(carts)
+            } else {
+                console.log(carts)
+            }
+        }).catch(next)
+    }
+
+    static cartHistory(req, res, next) {
+        const UserId = req.loginId;
+
+    }
 
 }
 

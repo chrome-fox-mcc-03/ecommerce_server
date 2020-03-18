09c5360739f2a08327d785dbe3702098c6b9669f
alvinarkansas <alvinarkansas@gmail.com>
2020-03-18 17:17:13 +0700

    authorization cleaned

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -18,26 +18,4 @@ module.exports = function(req, res, next) {
             console.log('errorrrrr disini');
             res.status(500).json(err)
         })
-    // Product.findOne({
-    //     where: {
-    //         id: req.params.id
-    //     }
-    // })
-    //     .then(product => {
-    //         if (product) {
-    //             if (product.UserId === req.currentUserId) {
-    //                 next();
-    //             } else {
-    //                 res.status(401).json({ status: 401, message: "Authorization failed" })
-    //             }
-    //         } else {
-    //             res.status(404).json({ status: 404, message: "Sorry, we do not find the product you're looking for" })
-    //         }
-    //         return null
-    //     })
-        // .catch(err => {
-        //     console.log('errorrrrr disini');
-            
-        //     res.status(500).json(err)
-        // })
 }

c0d00a637f0523d6452df9f92712e1363f8dfc3b
alvinarkansas <alvinarkansas@gmail.com>
2020-03-18 17:02:10 +0700

    authorization for admin added

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,28 +1,43 @@
-const { Product } = require('../models/index');
+const { User } = require('../models/index');
 
 module.exports = function(req, res, next) {
-    console.log('PARAMMMSSS ======> ', req.params.id);
-    
-    Product.findOne({
+    console.log(['Current User: '], req.currentUserId);
+    User.findOne({
         where: {
-            id: req.params.id
+            id: req.currentUserId
         }
     })
-        .then(product => {
-            if (product) {
-                if (product.UserId === req.currentUserId) {
-                    next();
-                } else {
-                    res.status(401).json({ status: 401, message: "Authorization failed" })
-                }
+        .then(user => {
+            if (user.is_admin) {
+                next()
             } else {
-                res.status(404).json({ status: 404, message: "Sorry, we do not find the product you're looking for" })
+                res.status(401).json({ status: 401, message: "Authorization failed" })
             }
-            return null
         })
         .catch(err => {
             console.log('errorrrrr disini');
-            
             res.status(500).json(err)
         })
+    // Product.findOne({
+    //     where: {
+    //         id: req.params.id
+    //     }
+    // })
+    //     .then(product => {
+    //         if (product) {
+    //             if (product.UserId === req.currentUserId) {
+    //                 next();
+    //             } else {
+    //                 res.status(401).json({ status: 401, message: "Authorization failed" })
+    //             }
+    //         } else {
+    //             res.status(404).json({ status: 404, message: "Sorry, we do not find the product you're looking for" })
+    //         }
+    //         return null
+    //     })
+        // .catch(err => {
+        //     console.log('errorrrrr disini');
+            
+        //     res.status(500).json(err)
+        // })
 }
--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const ProductController = require('../controllers/product');
 const authentication = require('../middlewares/authentication');
+const authorization = require('../middlewares/authorization');
 
 router.use(authentication);
 
-router.post('/', ProductController.add);
+router.post('/', authorization, ProductController.add);
 router.get('/', ProductController.findAll);
 router.get('/:id', ProductController.findOne);
-router.put('/:id', ProductController.update);
-router.delete('/:id', ProductController.delete);
+router.put('/:id', authorization, ProductController.update);
+router.delete('/:id', authorization, ProductController.delete);
 
 module.exports = router

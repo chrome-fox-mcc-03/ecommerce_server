e958efcf8050b61c97a42f0f778b201f16c85739
Juli Yanti <juleshwa@gmail.com>
2020-03-19 09:32:08 +0700

    revised ErrorHandler

--- middlewares/ErrorHandler.js
+++ middlewares/ErrorHandler.js
@@ -4,6 +4,23 @@ class ErrorHandler {
 
     static errHandling(err, req, res, next) {
 
+        // Error Token
+        if (err.name === 'JsonWebTokenError') {
+            err.status = 401;
+            err.message = `Please login`;
+        }
+
+        // Error Sequelize Validation
+        if (err.name === 'SequelizeValidationError') {
+            err.status = 400;
+            err.message = err.errors.map(each => each.message);
+        }
+
+        // Default Error Status 500
+        res
+            .status(err.status || 500)
+            .json({ message: err.message || `Internal Server Error` });
+
     }
 }
 

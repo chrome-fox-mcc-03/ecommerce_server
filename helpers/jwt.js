e88597f84a7d9a5308c79f550d61f81d214f15a7
ryanadhi <ryanadhi.p@gmail.com>
2020-03-21 12:17:04 +0700

    ready to deploy

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 function getToken (payload) {
-    return jwt.sign(payload, 'hacktivsecret')
+    return jwt.sign(payload, process.env.JWT_KEY)
 }
 
 function verifyToken (token ) {
-    return jwt.verify(token, 'hacktivsecret')
+    return jwt.verify(token, process.env.JWT_KEY)
 }
 
 module.exports = {

c2981db0691ba64fed8781f17a905333f492590a
ryanadhi <ryanadhi.p@gmail.com>
2020-03-21 12:29:39 +0700

    add route '/'

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ const ProductRouter = require ('./product') ;
 const AdminRouter =require('./admin') ;
 
 
+router.get('/', (req,res) => {
+    res.status(200).json({
+        message: "deploy success"
+    })
+})
 router.use('/admin', AdminRouter) ;
 router.use('/users', UserRouter) ;
 router.use('/products', ProductRouter) ;

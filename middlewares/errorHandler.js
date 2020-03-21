b2a58017033e11c0cd7cd342b2329734dd09a5dc
Andreas Anggara <andreasanggaraa@gmail.com>
2020-03-21 23:23:23 +0700

    implement error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,7 +3,7 @@ module.exports = function errorHandler(err, req, res, next) {
       let message = err.errors.map(el => el.message);
       res.status(400).json({message})
   }
-  if(err.name === "SequelizeDatabaseError") {
+  else if(err.name === "SequelizeDatabaseError") {
     let message = err.original
     res.status(400).json(message)
   }

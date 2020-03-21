85dd486aad9814fa960fa28d94a8448b21c0e254
andreasnw <andreasnw93@gmail.com>
2020-03-21 20:48:01 +0700

    config revised

--- bin/http.js
+++ bin/http.js
@@ -12,6 +12,6 @@ switch(env) {
 const app = require('../app');
 const http = require('http');
 const server = http.createServer(app);
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, ()=> console.log(`listening to PORT: ${PORT}`));

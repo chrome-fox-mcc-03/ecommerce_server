c4a66cefefd3b864bc706ec724816d9afaf39f23
maxhamz <maxwell.hamzah@gmail.com>
2020-03-21 16:16:26 +0700

    fixed bug in routes/index.js about getAllUsers

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const productRoute = require("./product.js")
 
 router.post("/register", UserController.register)
 router.post("/login", UserController.login)
-router.post("/getAllUsers", UserController.getAllUsers) // for testing
+router.get("/getAllUsers", UserController.getAllUsers) // for testing
 
 router.use("/products", productRoute)
 

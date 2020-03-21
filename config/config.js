8b6a26b371d5c4123ccc573e8b833404863b0dd9
xavierThufail <xavier.thufail@gmail.com>
2020-03-22 01:22:58 +0700

    add data heroku

--- config/config.js
+++ config/config.js
@@ -11,9 +11,5 @@ switch (env) {
 }
 
 module.exports = {
-  "username": process.env.DB_USER,
-  "password": process.env.DB_PASSWORD,
-  "database": process.env.DB_NAME,
-  "host": process.env.DB_HOST,
-  "dialect": process.env.DB_DIALECT,
+  "use_env_variable": "DATABASE_URL"
 }

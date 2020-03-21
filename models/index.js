d34340e2d13573683d36afe11ce725fb3c9ee9cf
Andreas Anggara <andreasanggaraa@gmail.com>
2020-03-21 17:28:00 +0700

    3rd try, solve on models/index.js

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.env.DATABASE_URL) {
+if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env[config.use_env_variable], {
     dialect: "postgres",
     protocol: "postgres"

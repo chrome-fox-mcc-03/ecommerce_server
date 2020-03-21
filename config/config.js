d23827175bf0b2421fc807bc0c683c4fbd394946
andreasnw <andreasnw93@gmail.com>
2020-03-21 21:08:14 +0700

    db name revised

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ switch(env) {
 module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
 }

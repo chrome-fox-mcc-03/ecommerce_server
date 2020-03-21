d26207dac3b6502a2a7270b5e8ecd144e11be016
alvinarkansas <alvinarkansas@gmail.com>
2020-03-21 14:52:35 +0700

    error handler fixed

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -8,10 +8,10 @@ module.exports = function (err, req, res, next) {
         let message = err.errors.map(error => error.message)
         res.status(400).json({ message })
     } else {
-        console.log('masuk sini nggak? _+_+_+_+');
-        console.log(err.status);
-        console.log(err.message);
-        res.status(err.status).json({ message: err.message })
-        // res.status(err.status || 500).json(err.message || 'Internal server error')
+        // console.log('masuk sini nggak? _+_+_+_+');
+        // console.log(err.status);
+        // console.log(err.message);
+        // res.status(err.status).json({ message: err.message })
+        res.status(err.status || 500).json(err.message || 'Internal server error')
     }
 }

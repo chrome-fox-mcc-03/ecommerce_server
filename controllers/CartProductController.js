030dfecbb2ed68f89d56d7feab672f9c16d430a8
Andreas Anggara <andreasanggaraa@gmail.com>
2020-03-26 12:03:33 +0700

    done remove

--- controllers/CartProductController.js
+++ controllers/CartProductController.js
@@ -1,13 +1,22 @@
-const { CartProduct, Product } = require('../models/index')
+const {
+  CartProduct,
+  Product
+} = require('../models/index')
 
 class CartProductController {
-  
+
   static findPerUser(req, res, next) {
     const id = +req.decoded.id
 
     CartProduct.findAll({
-      where: { CartId: id }, include: ['Cart', 'Product']
-    })
+        where: {
+          CartId: id
+        },
+        attributes: {
+          include: ['id']
+        },
+        include: ['Product']
+      })
       .then(response => {
         res.status(200).json(response)
       })
@@ -20,34 +29,88 @@ class CartProductController {
 
     const ProductId = +req.params.id
     const CartId = +req.decoded.id
-    const { quantity } = req.body
+    const {
+      quantity
+    } = req.body
     let addedItem = {}
 
-    Product.findOne({
-      where: { id: ProductId }
-    })
-      .then(response => {
-        addedItem.price = response.price
-        
-        if (response.stock >= quantity) {
-          addedItem.name = response.name
-          return CartProduct.create({
-            ProductId, quantity, CartId
-          })
-        } 
-        else {
-          return Promise.reject({
-            status: 400,
-            msg: "Sorry! You can't buy more than the available stock"
-          })
+    CartProduct.findOne({
+        where: {
+          CartId,
+          ProductId
         }
       })
       .then(response => {
-        addedItem.invoice = response.quantity * addedItem.price
-        res.status(201).json({addedItem})
+        if (response) {
+          addedItem.quantity = response.quantity
+          Product.findOne({
+              where: {
+                id: ProductId
+              }
+            })
+            .then(response => {
+              const total = Number(quantity) + addedItem.quantity
+              if (response.stock >= total) {
+                CartProduct.update({
+                    quantity: total
+                  }, {
+                    where: {
+                      CartId,
+                      ProductId
+                    }
+                  })
+                  .then(response => {
+                    res.status(200).json('selesai tambah ke cart yg sudah ada')
+                  })
+                  .catch(err => {
+                    next(err)
+                  })
+              } else {
+                return Promise.reject({
+                  status: 400,
+                  msg: "Sorry! You can't buy more than the available stock"
+                })
+              }
+            })
+            .catch(err => {
+              next(err)
+            })
+        } else {
+          Product.findOne({
+              where: {
+                id: ProductId
+              }
+            })
+            .then(response => {
+              addedItem.price = response.price
+
+              if (response.stock >= quantity) {
+                addedItem.name = response.name
+                return CartProduct.create({
+                  ProductId,
+                  quantity,
+                  CartId
+                })
+              } else {
+                return Promise.reject({
+                  status: 400,
+                  msg: "Sorry! You can't buy more than the available stock"
+                })
+              }
+            })
+            .then(response => {
+              addedItem.invoice = response.quantity * addedItem.price
+              res.status(201).json({
+                addedItem
+              })
+            })
+            .catch(err => {
+              next(err)
+            })
+        }
       })
       .catch(err => {
-        next(err)
+
       })
   }
 
@@ -55,11 +118,18 @@ class CartProductController {
     const CartId = +req.params.id
 
     CartProduct.destroy({
-      where: { id: CartId }
-    })
+        where: {
+          id: CartId
+        }
+      })
       .then(response => {
-        if(response) res.status(200).json({ msg: "Item(s) removed from cart" })
-        else next({ status: 404, msg: "Product not found"})
+        if (response) res.status(200).json({
+          msg: "Item(s) removed from cart"
+        })
+        else next({
+          status: 404,
+          msg: "Product not found"
+        })
       })
       .catch(err => {
         next(err)
@@ -69,24 +139,27 @@ class CartProductController {
   static pay(req, res, next) {
     const CartId = +req.params.id
     CartProduct.findOne({
-      where: { id: CartId }
-    })
+        where: {
+          id: CartId
+        }
+      })
       .then(response => {
-        if(response) {
+        if (response) {
           if (!response.isPaid) {
-            return CartProduct.update(
-              { isPaid: true },
-              { where: { id: CartId }}
-            )
-          }
-          else {
+            return CartProduct.update({
+              isPaid: true
+            }, {
+              where: {
+                id: CartId
+              }
+            })
+          } else {
             return Promise.reject({
               status: 400,
               msg: "You already pay this product"
             })
           }
-        }
-        else {
+        } else {
           return Promise.reject({
             status: 404,
             msg: "Product not found"
@@ -94,7 +167,9 @@ class CartProductController {
         }
       })
       .then(_ => {
-        res.status(200).json({ msg: "Thank you for purchasing our product!" })
+        res.status(200).json({
+          msg: "Thank you for purchasing our product!"
+        })
       })
       .catch(err => {
         next(err)
@@ -102,7 +177,7 @@ class CartProductController {
   }
 
 
-  
+
 }
 
 module.exports = CartProductController

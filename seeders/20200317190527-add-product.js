c048856fb59bd68f4940c581780ab7d674d1b78a
MickeyStrike <irfanmaulana281299@gmail.com>
2020-03-26 12:41:09 +0700

    update seeder product

--- seeders/20200317190527-add-product.js
+++ seeders/20200317190527-add-product.js
@@ -20,14 +20,6 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        name: 'Masker',
-        image_url: 'https://media.suara.com/pictures/480x260/2020/02/29/45914-ilustrasi-masker.jpg',
-        price: 10000,
-        stock: 200,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
         name: 'Detergen',
         image_url: 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/medium//90/MTA-3322188/daia_detergen-daia-620gr-bungkus-softener_full02.jpg',
         price: 30000,
@@ -40,6 +32,30 @@ module.exports = {
         image_url: 'https://d2pa5gi5n2e1an.cloudfront.net/global/images/product/beauty/Clean_and_Clear_Foaming_Face_Wash/Clean_and_Clear_Foaming_Face_Wash_L_1.jpg',
         price: 15000,
         stock: 15,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Wafer',
+        image_url: 'https://cf.shopee.co.id/file/370a8de609b016fa60222fcee0faac98',
+        price: 5000,
+        stock: 20,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Wafer',
+        image_url: 'https://www.lifull-produk.id/bundles/assets/img/product/selamat%20wafer%20chocolate%2060gr.jpg',
+        price: 5000,
+        stock: 20,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Wafer',
+        image_url: 'https://cdn.elevenia.co.id/g/4/4/0/5/4/6/18440546_B_V3.jpg',
+        price: 5000,
+        stock: 20,
         createdAt: new Date(),
         updatedAt: new Date()
       }

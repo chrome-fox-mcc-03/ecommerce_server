e0591a549379f19866c76b64740c244147e1a18f
xavierThufail <xavier.thufail@gmail.com>
2020-03-26 10:51:33 +0700

    change auth in product

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 const router = require('express').Router()
 const routerUser = require('./user')
 const routerProduct = require('./product')
-const authentication = require('../middlewares/authentication')
 
 router.get('/', (req, res) => {
   res.send('helloooo')
 })
 router.use('/user', routerUser)
-router.use(authentication)
 router.use('/product', routerProduct)
 
 module.exports = router
--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const Controller = require('../controllers/Product')
-const checkRole = require('../middlewares/checkRole')
 const authorization = require('../middlewares/authorization')
+const authentication = require('../middlewares/authentication')
 
 router.get('/', Controller.findAll)
-router.use(checkRole)
+router.use(authentication)
 router.post('/', Controller.create)
 router.patch('/:id', authorization, Controller.update)
 router.delete('/:id', authorization, Controller.deleteProduct)

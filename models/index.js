f8b22fb7b757ec453e274cd9b0dd548458a26b54
lukman baihaqi <lukmanbaihaqi78@gmail.com>
2021-06-01 22:02:58 +0700

    update heroku

--- models/index.js
+++ models/index.js
@@ -13,11 +13,6 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env[config.use_env_variable], {
     dialect: "postgres",
     protocol: "postgres",
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
   });
 } else {
   sequelize = new Sequelize(
@@ -35,7 +30,10 @@ fs.readdirSync(__dirname)
     );
   })
   .forEach((file) => {
-    const model = sequelize["import"](path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
